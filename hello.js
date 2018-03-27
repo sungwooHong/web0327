62bbb441d95e3832bcc403882158506a5c3f0f6b
Ki Yung Ahn <kyagrd@gmail.com>
2018-03-27 11:21:54 +0900

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+function requestCallbackFunction (req, res) {
+  // if(req.url === '/favicon.ico') {
+  //   console.log('Favicon was requested');
+  // } else {
+    console.log('hello');
+  // }
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}
+
+var server = http.createServer(
+  requestCallbackFunction
+);
+
+server.listen(8080);
